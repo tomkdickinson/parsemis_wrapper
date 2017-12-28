e17096fd00b86adf7cac48291dace0d3bace8b4a
Tom Dickinson <tomkdickinson@gmail.com>
2017-12-28 12:25:00 +0000

    Added in keyword arguments to control mining

--- parsemis/parsemis_wrapper.py
+++ parsemis/parsemis_wrapper.py
@@ -20,6 +20,7 @@ class FrequentGraph:
         self._graph = graph
         self._appears_in = appears_in
         self._support = len(self._appears_in)
+        self.__rank = None
 
     def to_string(self):
         if len(self._graph.edges()) == 0:
@@ -47,6 +48,13 @@ class FrequentGraph:
     def appears_in(self):
         return self._appears_in
 
+    @property
+    def rank(self):
+        return self.__rank
+
+    def set_rank(self, rank):
+        self.__rank = rank
+
 
 class ParsemisMiner:
     """
@@ -56,53 +64,12 @@ class ParsemisMiner:
     that are required for parsing.
     """
 
-    def __init__(self, data_location, parsemis_location=None, minimum_frequency="5%", close_graph=False,
-                 maximum_frequency=None, minimum_node_count=None, maximum_node_count=None, minimum_edge_count=None,
-                 maximum_edge_count=None, find_paths_only=False, find_trees_only=False, single_rooted=False,
-                 connected_fragments=True, algorithm="gspan", subdue=False, zaretsky=False, distribution="local",
-                 threads=1, store_embeddings=False, debug=False, mine_undirected=False):
-
+    def __init__(self, data_location, mine_undirected=True, debug=True):
         self.data_location = data_location
-        if parsemis_location is None:
-            self.parsemis_location = "%s/parsemis.jar" % os.path.dirname(os.path.realpath(__file__))
-        else:
-            self.parsemis_location = parsemis_location
 
+        self.parsemis_location = "%s/parsemis.jar" % os.path.dirname(os.path.realpath(__file__))
         os.makedirs(self.data_location, exist_ok=True)
 
-        self.minimum_frequency = minimum_frequency
-        self.maximum_frequency = maximum_frequency
-        if minimum_node_count is not None and type(minimum_node_count) is not int:
-            raise TypeError("Minimum node count must be an integer >= 0")
-        self.minimum_node_count = minimum_node_count
-        if maximum_node_count is not None and type(maximum_node_count) is not int:
-            raise TypeError("Maximum node count must be an integer >= 0")
-        self.maximum_node_count = maximum_node_count
-        if minimum_edge_count is not None and type(minimum_edge_count) is not int:
-            raise TypeError("Minimum edge count must be an integer >= 0")
-        self.minimum_edge_count = minimum_edge_count
-        if maximum_edge_count is not None and type(maximum_edge_count) is not int:
-            raise TypeError("Maximum edge count must be an integer >= 0")
-        self.maximum_edge_count = maximum_edge_count
-        self.find_paths_only = find_paths_only
-        self.find_trees_only = find_trees_only
-        self.single_rooted = single_rooted
-        self.connected_fragments = connected_fragments
-        allowed_algorithms = ["gspan", "gaston", "dagma"]
-        if algorithm.lower() in allowed_algorithms:
-            self.algorithm = algorithm.lower()
-        else:
-            raise Exception("Algorithm must be one of: %s" % ",".join(allowed_algorithms))
-        self.close_graph = close_graph
-        self.subdue = subdue
-        self.zaretsky = zaretsky
-        allowed_distributions = ["local", "threads", "threads_np"]
-        if distribution in allowed_distributions:
-            self.distribution = distribution
-        else:
-            raise Exception("Distribution must be: one of: %s" % ",".join(allowed_distributions))
-        self.threads = int(threads)
-        self.store_embeddings = store_embeddings
         if debug:
             self.debug_statement = "-Dverbose=true"
         else:
@@ -121,10 +88,10 @@ class ParsemisMiner:
         if os.path.exists(self.output_file):
             os.remove(self.output_file)
 
-    def mine_graphs(self, graphs):
+    def mine_graphs(self, graphs, **kwargs):
         log.debug("Mining %i graphs" % len(graphs))
         self.write_graph(graphs)
-        self.perform_mining()
+        self.perform_mining(**kwargs)
         return self.read_graph(graphs)
 
     def write_graph(self, graphs):
@@ -139,41 +106,40 @@ class ParsemisMiner:
         else:
             return self.read_lg()
 
-    def perform_mining(self):
+    def perform_mining(self, **kwargs):
         commands = ['java', '-jar',
                     "-Xmx10g",
                     self.parsemis_location,
                     "--graphFile=%s" % self.input_file,
                     "--outputFile=%s" % self.output_file,
-                    "--minimumFrequency=%s" % self.minimum_frequency,
-                    "--findPathsOnly=%s" % self.find_paths_only,
-                    "--findTreesOnly=%s" % self.find_trees_only,
-                    "--singleRooted=%s" % self.single_rooted,
-                    "--connectedFragments=%s" % self.connected_fragments,
-                    "--algorithm=%s" % self.algorithm,
-                    "--closeGraph=%s" % self.close_graph,
-                    "--subdue=%s" % self.subdue,
-                    "--zaretsky=%s" % self.zaretsky,
-                    "--distribution=%s" % self.distribution,
-                    "--threads=%s" % int(self.threads),
-                    "--storeEmbeddings=%s" % self.store_embeddings
+                    "--minimumFrequency=%s" % kwargs.get('minimum_frequency', '0.05'),
+                    "--findPathsOnly=%s" % kwargs.get('find_paths_only', True),
+                    "--findTreesOnly=%s" % kwargs.get('find_trees_only', True),
+                    "--singleRooted=%s" % kwargs.get('single_rooter', True),
+                    "--connectedFragments=%s" % kwargs.get('connectedFragments', True),
+                    "--algorithm=%s" % kwargs.get('algorithm', 'gspan'),
+                    "--closeGraph=%s" % kwargs.get('close_graph', False),
+                    "--subdue=%s" % kwargs.get('subdue', False),
+                    "--zaretsky=%s" % kwargs.get('zaretsky', False),
+                    "--distribution=%s" % kwargs.get('distribution', 'threads'),
+                    "--threads=%s" % kwargs.get('n_threads', 1),
+                    "--storeEmbeddings=%s" % kwargs.get('store_embeddings', True)
                     ]
         if self.debug_statement is not None:
             commands.insert(2, self.debug_statement)
 
-        if self.minimum_node_count is not None:
-            commands.append("--minimumNodeCount=%i" % self.minimum_node_count)
-        if self.maximum_node_count is not None:
-            commands.append("--maximumNodeCount=%i" % self.maximum_node_count)
-        if self.minimum_edge_count is not None:
-            commands.append("--minimumEdgeCount=%i" % self.minimum_edge_count)
-        if self.maximum_edge_count is not None:
-            commands.append("--maximumEdgeCount=%i" % self.maximum_edge_count)
-        if self.maximum_frequency is not None:
-            commands.append("--maximumFrequency=%i" % self.maximum_frequency)
+        if 'minimum_node_count' in kwargs:
+            commands.append("--minimumNodeCount=%i" % kwargs.get('minimum_node_count'))
+        if 'maximum_node_count' in kwargs:
+            commands.append("--maximumNodeCount=%i" % kwargs.get('maximum_node_count'))
+        if 'minimum_edge_count' in kwargs:
+            commands.append("--minimumEdgeCount=%i" % kwargs.get('minimum_edge_count'))
+        if 'maximum_edge_count' in kwargs:
+            commands.append("--maximumEdgeCount=%i" % kwargs.get('maximum_edge_count'))
+        if 'maximum_frequency' in kwargs:
+            commands.append("--maximumFrequency=%i" % kwargs.get('maximum_frequency'))
 
         log.debug(commands)
-        print(commands)
         subprocess.call(commands)
 
     def write_lg(self, graphs):
